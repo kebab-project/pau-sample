eb2edff2137260cf10e6b77bb6bb7d5e2bc7df62
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2014-03-28 16:32:20 +0200

    PSL-5 #time 10m project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pusula::Application.config.secret_key_base = 'b0e58525469ad8c7a321ad972b8739a985bd268ce8e47657baf43762e25250aec191bf0868ffb51450e6b832514d6f92e70b467d8803476ab8f98f98c71d21f8'
