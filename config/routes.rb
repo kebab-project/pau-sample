1989089d04b05dde156bca3f2d9f6e43808f6fe6
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2014-03-28 17:32:05 +0200

    PSL-7 PSL-2 #time 40m devise added and layout fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Pusula::Application.routes.draw do
+  devise_for :admins
+
+  namespace :admins do
+    resources :dashboard
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Admin.create(email: 'admin@pusula.com', password: 'pusulapusula', password_confirmation: 'pusulapusula')
